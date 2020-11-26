259c1ca9ced53eedbb740df095ce435d7a8320e0
BomF1 <74214721+BomF1@users.noreply.github.com>
2020-11-26 07:54:48 +0100

    Add files via upload

---
+++ cv/_partial/header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Curriculum Vitae</title>
+</head>
+<body>
+
+<div class="container">
+
+    <div class="sidebar fixed-top ">
+
+        <nav class="nav flex-column" id="nav">
+
+            <a href="index.php" class="nav-link">
+                <img src="img/me.jpg" alt="" class="rounded-circle main-foto">
+            </a>
+
+            <a class="nav-link" id="link" href="about_me.php">O mne</a>
+            <a class="nav-link" id="link" href="school.php">Štúdium</a>
+            <a class="nav-link" id="link" href="experience.php">Pracovné skúsenosti</a>
+            <a class="nav-link" id="link" href="hobbies.php">Záľuby</a>
+
+        </nav>
+    </div>
+
+</div>
